123a8504818f69472a15e01bb807b6a35d07d02a
BILTEK\386961 <bunyamnfdan@gmail.com>
2021-02-12 08:48:37 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+
+
+    {
+
+        //Constractır Oluşturuldu.
+        //Newleme yapılmadı. Bu şekilde direk DataAccess kısmına erişim sağlanıyor.
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları buraya gelecek
+            //Login olmuş ve yetkisi varsa artık DataAccess'a erişebilir.
+
+            return _productDal.GetAll();
+
+        }
+    }
+}
