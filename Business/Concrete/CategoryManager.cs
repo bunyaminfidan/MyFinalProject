b621a0e7b00577047aa3a8179b9643035998d66b
Bunyamin FIDAN <bunyamnfdan@gmail.com>
2021-02-13 23:29:42 +0300

    9. Gün C# Kurumsal Yazılım Mimarileri DTO yazmaya başlamadan öncesi

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categotyDal;
+
+        public CategoryManager(ICategoryDal categotyDal)
+        {
+            _categotyDal = categotyDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            //İş Kodları buraya yazılır.
+            //Yetkileri var mı yok mu? Sayı yeterli mi gibi gibi, fiyat 100'den az mı
+            return _categotyDal.GetAll();
+        }
+
+        //Select * from Categories where CategoryID=categoryId
+        public List<Category> GetById(int categoryId)
+        {
+            return _categotyDal.GetAll(c => c.CategoryId == categoryId);
+        }
+    }
+}
